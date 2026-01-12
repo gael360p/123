eb608cc1dffc213d0169e8c765cb3f759798216e
gael360p <145783386+gael360p@users.noreply.github.com>
2026-01-12 08:47:26 +0000

    Fix: Add missing STD_MULT constant to ChunkDataConfig

--- src/main/java/com/gael/chunkdata/ChunkDataConfig.java
+++ src/main/java/com/gael/chunkdata/ChunkDataConfig.java
@@ -1,11 +1,14 @@
 package com.gael.chunkdata;
 
 public final class ChunkDataConfig {
-  private ChunkDataConfig() {}
+    private ChunkDataConfig() {}
 
-  // master enable
-  public static boolean HEATMAP_ENABLED = true;
+    // master enable
+    public static final boolean HEATMAP_ENABLED = true;
 
-  // toggled by H and by command
-  public static boolean HEATMAP_TOGGLED = false;
+    // toggled by H and by command
+    public static boolean HEATMAP_TOGGLED = false;
+
+    // Add the missing multiplier constant
+    public static final double STD_MULT = 1.5; // Adjust value as appropriate
 }
